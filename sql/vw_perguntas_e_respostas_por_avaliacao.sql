ec320d34e449226b6fbebf052c54ee3457d52847
João Pedro da Costa <59283595+joaopedrodacostacarvalho@users.noreply.github.com>
2025-10-18 15:11:01 -0300

    Usando a tabela contem

--- sql/vw_perguntas_e_respostas_por_avaliacao.sql
+++ sql/vw_perguntas_e_respostas_por_avaliacao.sql
@@ -1,7 +1,8 @@
 CREATE OR REPLACE VIEW vw_perguntas_e_respostas_por_avaliacao AS
 select A.id_avaliacao, P.texto_pergunta, R.conteudo_resposta
-FROM pergunta P
+FROM avaliacao A
+JOIN contem C ON C.id_avaliacao = A.id_avaliacao
+JOIN pergunta P ON P.id_pergunta = C.id_pergunta
 JOIN resposta R ON R.id_pergunta = P.id_pergunta
-JOIN avaliacao A ON A.id_avaliacao = R.id_avaliacao
 GROUP BY A.id_avaliacao, P.id_pergunta, R.id_resposta
 ORDER BY A.id_avaliacao, P.texto_pergunta, R.conteudo_resposta;
